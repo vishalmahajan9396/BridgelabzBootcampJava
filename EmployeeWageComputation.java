a27623b9f1cafe8d6553d82f9116502358126f80
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-12 10:59:52 +0530

    Employee Present or Abscent Checked in EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,12 @@
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
+		int isPresent = 1;
+		//Computation
 		System.out.println("Welcome to Employee Wage Computation");
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isPresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abscent");
 	}
 }
