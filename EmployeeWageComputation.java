5fe2fb695301e9463d73002aeff5e59107f9ea75
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-12 11:13:54 +0530

    Daily Employee Wage was calculated in EmployeeWageComputaion.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,12 +1,19 @@
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
-		int isPresent = 1;
+		//Constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HR = 20;
+		//Variables
+		int empHrs = 0;
+		int empWage = 0;
 		//Computation
 		System.out.println("Welcome to Employee Wage Computation");
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == isPresent)
-			System.out.println("Employee is Present");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs = 8;
 		else
-			System.out.println("Employee is Abscent");
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Employee Wage: " + empWage);
 	}
 }
