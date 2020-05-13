a82e6967aa10312076742a66592be1157a4f5ac0
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-13 13:22:30 +0530

    [Vishal]Updated:Computed Employee Wage for Multiple Company in Procedural Way using Class Method

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,16 +1,14 @@
 public class EmployeeWageComputation {
+	//Constans
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth) {
 		//Variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
       //Computation
       System.out.println("Welcome to Employee Wage Computation");
-      while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_>
+      while (totalEmpHrs <=  maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
          totalWorkingDays++;
          int empCheck = (int) Math.floor(Math.random() * 10) % 3;
          switch(empCheck) {
@@ -26,12 +24,13 @@ public class EmployeeWageComputation {
          totalEmpHrs += empHrs;
          System.out.println("Day#: " + totalWorkingDays + "Employee Hours: " + empHrs);
       }
-      int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
-      System.out.println("Total Employee Wage:" + totalEmpWage);
+      int totalEmpWage = totalEmpHrs * empRatePerHours;
+      System.out.println("Total Employee Wage for Company:" +company+" is: "+ totalEmpWage);
 		return totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("DMart", 20, 20, 100);
+		computeEmpWage("Reliance", 10, 4, 50);
 	}
 }
